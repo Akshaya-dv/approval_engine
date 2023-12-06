adaa0e5ddf9ab53cade3df8fab37a7d90f01cd9c
Akashp-Sankey <akash.p@sankeysolutions.com>
2023-12-06 13:18:11 +0530

    integration of put-get-approver apis in jems

--- approval_engine/gems/urls.py
+++ approval_engine/gems/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProjectGeneration
+from .views import ProjectGeneration,ProjectApprover
 
 urlpatterns = [
    
 
    path('project-generate/',ProjectGeneration.as_view(), name='EmployeeLeaves'),   
+   path('project-approver/',ProjectApprover.as_view(), name='EmployeeLeaves'),   
     
 ]
